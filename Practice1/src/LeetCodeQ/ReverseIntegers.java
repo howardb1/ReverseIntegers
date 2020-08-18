48323695f805467f8c0c9694c3b062f5566c76ce
King_Chop27$ <howardb1@wit.edu>
2020-08-18 01:29:41 -0400

    second push

--- Practice1/src/LeetCodeQ/ReverseIntegers.java
+++ Practice1/src/LeetCodeQ/ReverseIntegers.java
@@ -1,10 +1,60 @@
 package LeetCodeQ;
 
 public class ReverseIntegers {
+	
 
+public int reverse (int x)	{
+	
+	int p = (int) Math.pow(2, 31);
+	int n = (int) Math.pow(-2, 31);
+	
+	if (x > p && x < n ) {
+		
+		return 0;
+	}
+	
+	String newstr = Integer.toString(x);
+	
+	
+	
+	
+	
+	
+	return 0;
+	
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		System.out.println("Input: ");
+		
+		
+		System.out.println("Output");
+		
 	}
 
 }
