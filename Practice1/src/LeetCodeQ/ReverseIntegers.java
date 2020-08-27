c1ad19248c4767c63cc874a2093079bf404878eb
King_Chop27$ <howardb1@wit.edu>
2020-08-27 01:58:08 -0400

    sixth push

--- Practice1/src/LeetCodeQ/ReverseIntegers.java
+++ Practice1/src/LeetCodeQ/ReverseIntegers.java
@@ -3,57 +3,59 @@ import java.util.Scanner;
 public class ReverseIntegers {
 	
 	
+	/*
+	 * public boolean containsChar (String s, char isNeg ) { isNeg = '-'; if
+	 * (s.length() == isNeg) { return true; } return false; }
+	 */
 	
 	
-	public static int reverse (int x)	{
 	
-	int p = (int) Math.pow(2, 31);
-	int n = (int) Math.pow(-2, 31);
+	public int reverse (int x)	{
 	
-	if (x > p && x < n ) {
+	int p = (int) Math.pow(2, 31);	//<--------- Declaring the numerical boundaries of the #'s that i only 
+	int n = (int) Math.pow(-2, 31);				// want to be used in the program
+	
+	if (x > p && x < n ) {			//<----------Making a case if those these boundaries are broken 
 		
-		return 0;
+		return 0;					//<----------This case will return a zero if if the the case up above is broken
 	}
 	
-	String str = Integer.toString(x);
+	String str = Integer.toString(x);   //<--------- This line is turn int x into a string of numbers
 	
-	StringBuilder sb = new StringBuilder(str);
+
+	
+	StringBuilder sb = new StringBuilder(str);		//<------- Declaring and initializing the StringBuilder using the string value "str", which as now turned into "sb"
+													//<------- so that i can use the reverse method on flip the order 
 	
 	
 
-	String newStr = sb.reverse().toString();
+	String newStr = sb.reverse().toString();		//<-------Using the reverse method from stringBuilder on renamed string value "sb" string that holds the #'s inna string 
 	
-	for (int i = 0; i <= str.length(); i++) {
-		if (str.charAt(i) == '-') {
-			return i;
-		}
-	}
+		
 	
 	
+	 int z = Integer.parseInt(newStr);				//<-------this line is essentially just turns the reversed string that 
+	 												//<------- holds the reversed numbers back into an int value 
 	
-	int z = Integer.parseInt(newStr);
-	
-	
-	
-	
-	
-	
-	return z;
-	
-}
+			return z;
+		
+} 
 
+	
 
 	
 	
 	public static void main(String[] args) {	
 	Scanner sc = new Scanner(System.in);
+	ReverseIntegers ri = new ReverseIntegers();
+	
+	
 	
 	System.out.print("Input: ");
 		int num = sc.nextInt();
-		int revNum = reverse(num);
+		ri.reverse(num);
 		
-		
-	System.out.println("Output: " + revNum);
+	System.out.println("Output: " + ri.reverse(num));
 		
 		
 		
